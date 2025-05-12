0933ba9869221b48e7ba1e34e9df5b2856b0f4a5
juanalpz <lpz_juana@hotmail.com>
2025-05-12 00:57:17 -0300

    Ejercicio 1

--- ejercicio1/Main.java
+++ ejercicio1/Main.java
@@ -2,20 +2,17 @@ public class Main {
     public static void main(String[] args) {
         ConjuntoEspecialTDA conjunto = new ConjuntoEspecialTDA();
 
-        // Valores de prueba: 18, 2, 7, 4, 18, 22, 2, 7
         conjunto.inicializarConjunto();
-        conjunto.agregar(18);
-        conjunto.agregar(2);
-        conjunto.agregar(7);
-        conjunto.agregar(4);
-        conjunto.agregar(18);
-        conjunto.agregar(22);
-        conjunto.agregar(2);
-        conjunto.agregar(7);
 
-        // Mostrar los resultados
-        System.out.println("Elementos en el conjunto: ");
-        for (int i = 0; i < 23; i++) {
+        // Valores de prueba: 18, 2, 7, 4, 18, 22, 2, 7
+        int[] datos = {18, 2, 7, 4, 18, 22, 2, 7};
+        for (int i = 0; i < datos.length; i++) {
+            conjunto.agregar(datos[i]);
+        }
+
+        // Muestra los valores únicos del conjunto
+        System.out.println("Elementos únicos en el conjunto:");
+        for (int i = 0; i <= 100; i++) {
             if (conjunto.pertenece(i)) {
                 System.out.println(i);
             }
